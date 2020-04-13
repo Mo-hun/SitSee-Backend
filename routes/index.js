e721982c1a636416c6edaf0470cda30f8bc309b4
Mo-hun <twicemomo@kakao.com>
2020-04-14 00:11:40 +0900

    [UPDATE] Domain Update

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+console.log('webserver');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if(req.host =="xnxn--hs-k51jh73aj8b.com"){
+    res.view('domain');
+  }
   res.json({ message : 'fuck you!' });
 });
 
