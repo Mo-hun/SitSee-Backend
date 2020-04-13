545a2de80febd94b2a7262fc27d727dc0572acdf
Mo-hun <twicemomo@kakao.com>
2020-04-14 00:20:02 +0900

    [UPDATE] res.view --> res.render

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ console.log('webserver');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(req.host =="www.xn--hs-k51jh73aj8b.com" || req.host == "xn--hs-k51jh73aj8b.com"){
-    res.view('domain');
+    res.render('domain');
   }
   res.json({ message : 'fuck you!' });
 });
