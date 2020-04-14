f4d08ca974329b03567ac14ac0e2fb16cbe27955
Mo-hun <twicemomo@kakao.com>
2020-04-14 10:55:03 +0900

    [UPDATE] Agree CORS

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 console.log('webserver');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(req.host =="www.xn--hs-k51jh73aj8b.com" || req.host == "xn--hs-k51jh73aj8b.com"){
+  if(req.host == "www.xn--hs-k51jh73aj8b.com" || req.host == "xn--hs-k51jh73aj8b.com"){
     res.render('domain');
   }
   res.json({ message : 'fuck you!' });
